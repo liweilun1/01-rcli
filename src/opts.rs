d920bf3f98788a196e96bf043fd16853daf67435
liweilun1 <935671855@qq.com>
2024-04-19 17:54:59 +0800

    di 8 jiang

--- src/opts.rs
+++ src/opts.rs
@@ -10,6 +10,8 @@ pub struct Opts {
 pub enum SubCommand {
     #[command(name = "csv", about = "Show Csv ,or convert csv to other  format")]
     Csv(CsvOpts),
+    #[command(name = "genpass", about = "Generate a random Password")]
+    GenPass(GenPassOpts),
 }
 #[derive(Debug, Clone, Copy)]
 pub enum OutputFormat {
@@ -29,6 +31,23 @@ pub struct CsvOpts {
     pub delimiter: char,
     #[arg(long, default_value_t = true)]
     pub header: bool,
+}
+#[derive(Debug, Parser)]
+pub struct GenPassOpts {
+    #[arg(short, long, default_value_t = 16)]
+    pub length: u8,
+
+    #[arg(long, default_value_t = true)]
+    pub uppercase: bool,
+
+    #[arg(long, default_value_t = true)]
+    pub lowercase: bool,
+
+    #[arg(long, default_value_t = true)]
+    pub number: bool,
+
+    #[arg(long, default_value_t = true)]
+    pub symbol: bool,
 }
 fn verify_input_file(filename: &str) -> Result<String, &'static str> {
     if Path::new(filename).exists() {
